00131a6b97f1097fc146deeb8e7207198bf4bcff
DJDims <djdims123@mail.ru>
2021-09-16 00:11:36 +0300

    Новое удаление мин и макс значений

--- src/evenarray/EvenArray.java
+++ src/evenarray/EvenArray.java
@@ -45,37 +45,27 @@ public class EvenArray {
 
         //----- Среднее
         
-        int countofmax = 0;
+        int countofmax = 0;                         //количество максимальных значений
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == max) {
                 countofmax++;
             }
-            
         }
-        System.out.println("Количество максимальных значений"+countofmax);
         
-        int countofmin = 0;
+        int countofmin = 0;                         //количество минимальных значений
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == min) {
                 countofmin++;
             }
-            
         }
-        System.out.println("Количество минимальных значений"+countofmin);
         
         int[] newnumbers = new int[numbers.length-(countofmin+countofmax)];
-                System.arraycopy(numbers, countofmin, newnumbers, 0, 6);
-        System.out.println(Arrays.toString(newnumbers));
-        
-//        int mid = 0;
-//        int count = 0;
-//        for (int i = 0; i < numbers.length; i++) {
-//            if (numbers[i] == max || numbers[i] == min) {
-//                continue;
-//            }
-//            mid += numbers[i];
-//            count++;
-//        }
-//        System.out.println("Среднее = " + mid / count);
+        Arrays.sort(numbers);
+        System.arraycopy(numbers, countofmin, newnumbers, 0, newnumbers.length);
+        float mid = 0;
+        for (int i = 0; i < newnumbers.length; i++) {
+            mid += newnumbers[i];
+        }
+        System.out.println("Среднее = " + mid / newnumbers.length);
     }
 }
